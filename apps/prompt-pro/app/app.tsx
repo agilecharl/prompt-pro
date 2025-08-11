1f8b3b714510596c400849379cdb66ff5ba919cc
agilecharl <agilecharl@gmail.com>
2025-08-11 10:25:20 +0200

    Initial commit

---
+++ apps/prompt-pro/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@prompt-pro/prompt-pro" />
+    </div>
+  );
+}
+
+export default App;
